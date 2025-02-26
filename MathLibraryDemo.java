8e4bb3be0f28b332d5269c58d8041743d199d28a
angge1203 <jamisalangelic_bscs@plmun.edu.ph>
2025-02-26 23:02:41 +0800

    Submitting OBJPROG Week 02 - Session 01 - Exercise 04

---
+++ MathLibraryDemo.java
@@ -0,0 +1,15 @@
+public class MathLibraryDemo {
+    public static void main(String[] args) {
+double base = 2;
+int exponent = 3;
+double powerResult = Math.pow(base, exponent);
+double numberForSqrt = 16;
+double sqrtResult = Math.sqrt(numberForSqrt);
+double randomNumber = Math.random();
+System.out.println("Power: " + powerResult);
+System.out.println("Square Root: " + sqrtResult);
+System.out.println("Rounded Number: " + roundedNumber);
+System.out.println("Random Number: " + randomNumber);
+
+    }
+}
